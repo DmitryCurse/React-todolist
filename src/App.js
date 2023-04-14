17a9afdb05563bf6929174c6c59c315b96a6d242
DmitryCurse <dimonil488@gmail.com>
2023-04-14 21:57:36 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addNewTodo, fetchTodos } from "./store/todoSlice";
+
+import "./App.css";
+
+import TodoList from "./components/TodoList";
+import InputField from "./components/InputField";
+
+function App() {
+  const [text, setText] = useState("");
+  const { status, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    if (text.trim().length) dispatch(addNewTodo(text));
+    setText("");
+  };
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+
+      {status === "loading" && <h2>Loading...</h2>}
+      {error && <h2>An error occured: {error}</h2>}
+
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
